7fd121b18f97d0e8fd31fbcce627e462b3ec6607
tianh5 <tianh5@xiaopeng.com>
2024-12-03 09:51:02 +0800

    init

---
+++ src/main/java/org/cycle/ai/study/SpringAiStudyApplication.java
@@ -0,0 +1,13 @@
+package org.cycle.ai.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiStudyApplication.class, args);
+    }
+
+}
